4468235cf3f55fb4d6f4efbc64cc0813246c24e7
com02 <cbsorrilha@hotmail.com>
2016-04-11 16:31:17 -0300

    Nothing relevant. Just maintaining the repo up-to-date

--- js/main.js
+++ js/main.js
@@ -1,77 +1,115 @@
+/*global $, console, window, document */
 function ElMapper() {
-  var Mapper = this;
-  Mapper.prmBtn = $("#primary-menu-btn");
-  Mapper.hdnMenu = $(".hidden-menu");
-  Mapper.closeBtn = $('#close-menu-btn');
-  Mapper.nav = $('.nav-links');
-
+    "use strict";
+    var Mapper = this;
+    Mapper.prmBtn = $("#primary-menu-btn");
+    Mapper.hdnMenu = $(".hidden-menu");
+    Mapper.closeBtn = $('#close-menu-btn');
+    Mapper.nav = $('.nav-links');
+    Mapper.quote = $('#quote');
+    Mapper.quoteAuthor = $('#quote-author');
 }
 function HiddenNShowController() {
+    "use strict";
     var Cont = this;
     Cont.Mapper = new ElMapper();
 
     Cont.hideMenuAndCloseBtn = function (callback) {
-      Cont.Mapper.prmBtn.off().removeClass('hidden');
-      Cont.Mapper.hdnMenu.addClass('hidden');
-      Cont.Mapper.closeBtn.addClass('hidden');
-      callback();
-    }
+        Cont.Mapper.prmBtn.off().removeClass('hidden');
+        Cont.Mapper.hdnMenu.addClass('hidden');
+        Cont.Mapper.closeBtn.addClass('hidden');
+        callback();
+    };
 
     Cont.showMenuAndCloseBtn = function (callback) {
 
-      Cont.Mapper.prmBtn.addClass('hidden');
-      Cont.Mapper.hdnMenu.removeClass('hidden');
-      Cont.Mapper.closeBtn.off().removeClass('hidden');
-      callback();
-    }
+        Cont.Mapper.prmBtn.addClass('hidden');
+        Cont.Mapper.hdnMenu.removeClass('hidden');
+        Cont.Mapper.closeBtn.off().removeClass('hidden');
+        callback();
+    };
 }
 
 function EventBinder() {
+    "use strict";
 
-  var Binder = this,
-      HnSCont = new HiddenNShowController();
-  Binder.mapper = new ElMapper();
+    var Binder = this,
+        HnSCont = new HiddenNShowController();
 
-  Binder.offEverything = function () {
-    Binder.mapper.prmBtn.off();
-    Binder.mapper.closeBtn.off();
-  }
+    Binder.mapper = new ElMapper();
 
-  Binder.bindNavLinks = function () {
-    Binder.mapper.nav.each(function () {
-      $(this).click(function () {
-        HnSCont.hideMenuAndCloseBtn(Binder.bindPrmClick);
-      });
-    });
-  }
+    Binder.offEverything = function () {
+        Binder.mapper.prmBtn.off();
+        Binder.mapper.closeBtn.off();
+    };
 
-  Binder.bindPrmClick = function () {
-    $(this).off();
-    Binder.mapper.prmBtn.click(function () {
-      HnSCont.showMenuAndCloseBtn(Binder.bindCloseClick);
-    });
+    Binder.bindNavLinks = function () {
+        Binder.mapper.nav.each(function () {
+            $(this).click(function () {
+                HnSCont.hideMenuAndCloseBtn(Binder.bindPrmClick);
+            });
+        });
+    };
 
-  }
-  Binder.bindCloseClick = function () {
-    $(this).off();
-    Binder.mapper.closeBtn.click(function () {
-      HnSCont.hideMenuAndCloseBtn(Binder.bindPrmClick);
-    });
+    Binder.bindPrmClick = function () {
+        $(this).off();
+        Binder.mapper.prmBtn.click(function () {
+            HnSCont.showMenuAndCloseBtn(Binder.bindCloseClick);
+        });
+    };
+    Binder.bindCloseClick = function () {
+        $(this).off();
+        Binder.mapper.closeBtn.click(function () {
+            HnSCont.hideMenuAndCloseBtn(Binder.bindPrmClick);
+        });
+    };
 
-  }
-
-  Binder.bind = function () {
-    Binder.bindPrmClick();
-    Binder.bindNavLinks();
-  }
+    Binder.bind = function () {
+        Binder.bindPrmClick();
+        Binder.bindNavLinks();
+    };
 
 }
 
-$(document).ready(function(){
+function QuoteGenerator() {
+    "use strict";
+    var app = this;
 
-  var Binder = new EventBinder();
+    app.quotesData = [];
+    app.mapper = new ElMapper();
 
-  Binder.bind();
+    app.init = function () {
+        $.when({}).then(app.getQuote).then(app.generate);
+    };
+    app.generate = function () {
+        var position = Math.floor(Math.random() * (app.quotesData.length - 1)),
+            quote = app.quotesData[position];
 
+        app.mapper.quote.html(quote.text);
+        app.mapper.quoteAuthor.html(quote.author);
+
+    };
+    app.getQuote = function () {
+        var defer = $.Deferred();
+        $.get('quotes.json').then(function (data) {
+            console.log(data);
+            app.quotesData = data.quotes;
+            defer.resolve();
+        });
+        return defer.promise();
+    };
+
+    return app;
+
+}
+
+$(document).ready(function () {
+    "use strict";
+
+    var Binder = new EventBinder(),
+        QGenerator = new QuoteGenerator();
+
+    Binder.bind();
+    QGenerator.init();
 
 });
