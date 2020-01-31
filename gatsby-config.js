c2724be08cf08e86710669fe6d5151b6c0be60a4
Cesar de Barros <cbsorrilha@hotmail.com>
2020-01-31 10:58:16 -0300

    post: um argumento egoista pra ensinar seu amigo a programar

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
         short_name: `dbarros`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#333`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
       },
