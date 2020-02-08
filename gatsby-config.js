b538cb2558a8e50749776c4e182580ab2af8cced
Cesar de Barros <cbsorrilha@hotmail.com>
2020-02-08 12:22:54 -0300

    feat:  not found page

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,11 +106,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    {
-      "moduleNameMapper": {
-        "/.+\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/": "./__mocks__/file-mock.js"
-      },
-      "resolver": undefined
-    }
   ],
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,20 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import { rhythm } from "../utils/typography"
+
+const NotFoundTitle = styled.h1`
+  grid-column: 2 / 14;
+  text-align: center;
+  font-size: ${rhythm(1.5)};
+`;
+
+const NotFoundParagraph = styled.p`
+  grid-column: 2 / 14;
+  text-align: center;
+  font-size: ${rhythm(.75)};
+`;
 
 export default function NotFoundPage({ data, location }) {
   const siteTitle = data.site.siteMetadata.title;
@@ -10,9 +24,9 @@ export default function NotFoundPage({ data, location }) {
   return (
     <Layout location={location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Não encontrado</h1>
-        <p>Hmmm... Acho que você não deveria estar aqui.</p>
-      </Layout>
+        <NotFoundTitle>Não encontrado</NotFoundTitle>
+        <NotFoundParagraph>Hmmm... Acho que você não deveria estar aqui.</NotFoundParagraph>
+    </Layout>
   )
 }
 
