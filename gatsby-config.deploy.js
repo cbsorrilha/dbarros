7c87a044582496e5d88a0f56ab19b4c69264de60
Cesar de Barros <cbsorrilha@hotmail.com>
2020-02-09 10:35:56 -0300

    feat: firebase analytics

--- gatsby-config.deploy.js
+++ gatsby-config.deploy.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: '{{GATSBY_FIREBASE_MEASUREMENT_ID}}',
       },
     },
     `gatsby-plugin-feed`,
@@ -113,15 +113,15 @@ module.exports = {
           analytics: true,
         },
         credentials: {
-          apiKey: 'YOUR_GATSBY_FIREBASE_API_KEY',
-          authDomain: 'YOUR_GATSBY_FIREBASE_AUTH_DOMAIN',
-          databaseURL: 'YOUR_GATSBY_FIREBASE_DATABASE_URL',
-          projectId: 'YOUR_GATSBY_FIREBASE_PROJECT_ID',
-          storageBucket: 'YOUR_GATSBY_FIREBASE_STORAGE_BUCKET',
-          messagingSenderId: 'YOUR_GATSBY_FIREBASE_MESSAGING_SENDER_ID',
-          appId: 'YOUR_GATSBY_FIREBASE_APP_ID',
-          measurementId: 'YOUR_GATSBY_FIREBASE_MEASUREMENT_ID',
-        },
+          apiKey: '{{GATSBY_FIREBASE_API_KEY}}',
+          authDomain: '{{GATSBY_FIREBASE_AUTH_DOMAIN}}',
+          databaseURL: '{{GATSBY_FIREBASE_DATABASE_URL}}',
+          projectId: '{{GATSBY_FIREBASE_PROJECT_ID}}',
+          storageBucket: '{{GATSBY_FIREBASE_STORAGE_BUCKET}}',
+          messagingSenderId: '{{GATSBY_FIREBASE_MESSAGING_SENDER_ID}}',
+          appId: '{{GATSBY_FIREBASE_APP_ID}}',
+          measurementId: '{{GATSBY_FIREBASE_MEASUREMENT_ID}}',
+        }
       },
     }
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
-import React from "react"
+import React, { useEffect, useContext } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Bio from "../components/bio"
+import { FirebaseContext } from "gatsby-plugin-firebase"
 
 
 export default function BlogIndex({ location, data }) {
   const siteTitle = data.site.siteMetadata.title
+  const firebase = useContext(FirebaseContext)
+
+  useEffect(() => {
+    if (!firebase) {
+      console.error('firebase not found')
+      return
+    }
+    
+    firebase
+      .analytics()
+      .logEvent("visited_index")
+  }, [firebase])
 
   return (
     <Layout location={location} title={siteTitle}>
